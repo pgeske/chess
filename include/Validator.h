b93040d7c7ed693a9a297a79caf6f1b8d98cab36
Philip Geske <pwg46@cornell.edu>
2017-07-03 19:30:15 -0400

    Adding private validation functions for each piece.

--- include/Validator.h
+++ include/Validator.h
@@ -10,6 +10,13 @@ class Validator {
         ~Validator();
         static bool validate(Board *board, int r1, int c1, int r2, int c2);
         std::vector<Square*> allowed(Board *board, int r1, int c1);
+    private:
+        std::vector<Square*> allowedPawn(Board *board, int r1, int c1);
+        std::vector<Square*> allowedRook(Board *board, int r1, int c1);
+        std::vector<Square*> allowedBishop(Board *board, int r1, int c1);
+        std::vector<Square*> allowedKnight(Board *board, int r1, int c1);
+        std::vector<Square*> allowedQueen(Board *board, int r1, int c1);
+        std::vector<Square*> allowedKing(Board *board, int r1, int c1);
 };
 
 #endif
--- src/Validator.cpp
+++ src/Validator.cpp
@@ -1,4 +1,5 @@
 #include "Validator.h"
+#include "Piece.h"
 
 /**
  * Returns true if move on board->board[r1][c1] to board->board[r2][c2]
@@ -13,9 +14,28 @@ bool Validator::validate(Board *board, int r1, int c1, int r2, int c2) {
  * can travel to.
  */
 std::vector<Square*> Validator::allowed(Board *board, int r1, int c1) {
+    std::vector<Square*> allowedSquares;
+    Piece *piece = board->board[r1][c1]->piece;
+    // No piece
+    if (!piece) {
+        return allowedSquares;
+    }
     // Pawn
+    else if (piece->type == PieceType::Pawn) {
+    }
     // Rook
+    else if (piece->type == PieceType::Rook) {
+    }
     // Bishop
+    else if (piece->type == PieceType::Bishop) {
+    }
     // Queen
+    else if (piece->type == PieceType::Queen) {
+    }
     // King -- Trickier
+    return allowedSquares;
+}
+
+std::vector<Square*> Validator::allowedPawn(Board *board, int r1, int c1) {
+    Piece *piece = board->board[r1][c1]->piece;
 }
